8f5190cb74564347802dda3573984a585c88767a
Erik Barzdukas <erik.bardzdukas@gmail.com>
2015-07-29 16:28:58 -0700

    Forgot what changed in the Gruntfile once I staged it

--- Gruntfile.js
+++ Gruntfile.js
@@ -80,6 +80,7 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-mocha-test');
   grunt.loadNpmTasks('grunt-shell');
   grunt.loadNpmTasks('grunt-nodemon');
+  grunt.loadNpmTasks('grunt-processhtml');
 
   grunt.registerTask('server-dev', function (target) {
     // Running nodejs in a different process and displaying output on the main console
