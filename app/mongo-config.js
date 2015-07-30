d2e0a3f2611c98d27bd1f6dad0fa513a5c928ce6
Erik Barzdukas <erik.bardzdukas@gmail.com>
2015-07-30 11:40:56 -0700

    Moved schema definitions into callback on connection to mongo db

--- app/mongo-config.js
+++ app/mongo-config.js
@@ -1,19 +1,28 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
-var urlSchema = new Schema({
-  url: String,
-  base_url: String,
-  code: String,
-  title: String,
-  visits: Number,
-  created_at: {time: Date, default: Date.now}
+mongoose.connect(/*Mongo instanze on Azure*/, function(err){
+  if(err) console.log("Mongo error ", err);
+
+  /**
+   * Might not work b/c 'exports' value in anon func
+   * could differ from invocation context
+   */
+  exports.urlSchema = new Schema({
+    url: String,
+    base_url: String,
+    code: String,
+    title: String,
+    visits: Number,
+    created_at: {time: Date, default: Date.now}
+  });
+
+  exports.userSchema = new Schema({
+    username: String,
+    password: String,
+    created_at: {time: Date, default: Date.now}
+  });
 });
 
-var userSchema = new Schema({
-  username: String,
-  password: String,
-  created_at: {time: Date, default: Date.now}
-});
 
 
